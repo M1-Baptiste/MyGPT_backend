3670bfbe771e79f7b253d34effbbfd77cafc2cee
Baptiste <34516070+BaptisteLonguepee@users.noreply.github.com>
2025-05-06 10:10:56 +0200

    First commit

---
+++ src/test/java/com/example/mygpt/MyGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
